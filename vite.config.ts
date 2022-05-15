443a127201e4a8bee389820a31d34af23cb19dff
logeast <logeast@outlook.com>
2022-05-16 06:51:31 +0800

    chore: update packages descriptions

--- vite.config.ts
+++ vite.config.ts
@@ -9,17 +9,15 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'packages/index.tsx'),
       name: 'tailwind-layout-react',
-      fileName: (format) => `tailwind-layout-react.${format}.js`,
+      fileName: format => `tailwind-layout-react.${format}.js`
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['react', 'autoprefixer', 'postcss', 'tailwindcss'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          react: 'React',
-        },
-      },
-    },
-  },
+          react: 'React'
+        }
+      }
+    }
+  }
 });
