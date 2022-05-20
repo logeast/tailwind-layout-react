bdd31dbb236fd8f1c3366e70ab43dffa2322b948
logeast <logeast@outlook.com>
2022-05-20 10:34:03 +0800

    feat: update filter types and classNames

--- packages/utils/classes.ts
+++ packages/utils/classes.ts
@@ -1,14 +1,28 @@
 /**
  *
  * @param value Target type in the props
- * @param prefix Prefix string of the classname
- * @returns The result classname
+ * @param prefix Prefix string of the className
+ * @returns The result className
  */
 export function stringOrBooleanCls(value?: string | boolean, prefix?: string) {
   const isBoolean = typeof value === 'boolean';
   let result;
-  if (value) {
+  if (value !== undefined) {
     result = isBoolean ? prefix : `${prefix}-${value}`;
+  }
+  return result;
+}
+
+/**
+ * 
+ * @param value Target type in the props
+ * @param cls False and True className
+ * @returns The result className
+ */
+export function booleanCls(value?: boolean, cls?: {trueCls?: string, falseCls?: string}) {
+  let result;
+  if (value !== undefined) {
+    result = value ? cls?.trueCls : cls?.falseCls;
   }
   return result;
 }
--- packages/utils/types/filter-types.tsx
+++ packages/utils/types/filter-types.tsx
@@ -1,3 +1,7 @@
+import classNames from 'classnames';
+import { stringOrBooleanCls, booleanCls } from '../classes';
+
+// #region [Single Type]
 export declare type BlurType = 'none' | 'sm' | true | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
 
 export declare type BrightnessType = '0' | '50' | '75' | '90' | '95' | '100' | '105' | '110' | '125' | '150' | '200';
@@ -59,3 +63,77 @@ export declare type BackdropOpacityType =
 export declare type BackdropSaturateType = '0' | '50' | '100' | '150' | '200';
 
 export declare type BackdropSepiaType = boolean;
+
+// #endregion
+
+// #region [Components Props]
+export interface EffectsProps {
+  blur?: BlurType;
+  brightness?: BrightnessType;
+  contrast?: ContrastType;
+  dropShadow?: DropShadowType;
+  grayscale?: GrayscaleType;
+  hueRotate?: HueRotateType;
+  invert?: InvertType;
+  saturate?: SaturateType;
+  sepia?: SepiaType;
+  backdropBlur?: BackdropBlurType;
+  backdropBrightness?: BackdropBrightnessType;
+  backdropContrast?: BackdropContrastType;
+  backdropGrayscale?: BackdropGrayscaleType;
+  backdropHueRotate?: BackdropHueRotateType;
+  backdropInvert?: BackdropInvertType;
+  backdropOpacity?: BackdropOpacityType;
+  backdropSaturate?: BackdropSaturateType;
+  backdropSepia?: BackdropSepiaType;
+}
+// #endregion
+
+// #region [Components ClassNames]
+export const EffectsClassNames = (props: EffectsProps) => {
+  const {
+    blur,
+    brightness,
+    contrast,
+    dropShadow,
+    grayscale,
+    hueRotate,
+    invert,
+    saturate,
+    sepia,
+    backdropBlur,
+    backdropBrightness,
+    backdropContrast,
+    backdropGrayscale,
+    backdropHueRotate,
+    backdropInvert,
+    backdropOpacity,
+    backdropSaturate,
+    backdropSepia,
+  } = props;
+
+  return classNames(
+    stringOrBooleanCls(blur, 'blur'),
+    stringOrBooleanCls(backdropBlur, 'backdrop-blur'),
+    stringOrBooleanCls(dropShadow, 'drop-shadow'),
+    booleanCls(grayscale, { trueCls: 'grayscale', falseCls: 'grayscale-0' }),
+    booleanCls(invert, { trueCls: 'invert', falseCls: 'invert-0' }),
+    booleanCls(sepia, { trueCls: 'sepia', falseCls: 'sepia-0' }),
+    booleanCls(backdropGrayscale, { trueCls: 'backdrop-grayscale', falseCls: 'backdrop-grayscale-0' }),
+    booleanCls(backdropInvert, { trueCls: 'backdrop-invert', falseCls: 'backdrop-invert-0' }),
+    booleanCls(backdropSepia, { trueCls: 'sepia-grayscale', falseCls: 'sepia-grayscale-0' }),
+    {
+      [`brightness-${brightness}`]: brightness,
+      [`contrast-${contrast}`]: contrast,
+      [`drop-shadow-${dropShadow}`]: dropShadow,
+      [`hue-rotate-${hueRotate}`]: hueRotate,
+      [`saturate-${saturate}`]: saturate,
+      [`backdrop-brightness-${backdropBrightness}`]: backdropBrightness,
+      [`backdrop-contrast-${backdropContrast}`]: backdropContrast,
+      [`backdrop-hue-rotate-${backdropHueRotate}`]: backdropHueRotate,
+      [`backdrop-opacity-${backdropOpacity}`]: backdropOpacity,
+      [`backdrop-saturate-${backdropSaturate}`]: backdropSaturate,
+    }
+  );
+};
+// #endregion
