bc35d30bf29cee04eef485f3b8e39aeb429af937
logeast <logeast@outlook.com>
2022-06-01 07:25:32 +0800

    feat: initial gh-pages

--- gh-pages/next.config.js
+++ gh-pages/next.config.js
@@ -1,26 +1,6 @@
-const withPlugins = require("next-compose-plugins");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx$/,
-});
-
-module.exports = withPlugins(
-  [
-    [
-      withMDX,
-      {
-        pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-      },
-    ],
-  ],
-  {
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.resolve.fallback.fs = false;
-      }
-      return config;
-    },
-    reactStrictMode: true,
-  }
-);
+module.exports = nextConfig
